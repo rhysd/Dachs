56df879f82cf81df8391b87d89ca0f2c33635514
rhysd <lin90162@yahoo.co.jp>
2014-03-20 03:35:18 +0900

    add skeleton of parser using boost::spirit::qi

--- src/parser.cpp
+++ src/parser.cpp
@@ -1,11 +1,75 @@
+#define BOOST_SPIRIT_USE_PHOENIX_V3
+#define BOOST_RESULT_OF_USE_DECLTYPE 1
+
+#include <string>
+#include <memory>
+#include <exception>
+#include <cstddef>
+
+#include <boost/spirit/include/qi.hpp>
+#include <boost/spirit/include/qi_as.hpp>
+#include <boost/spirit/include/phoenix_core.hpp>
+#include <boost/spirit/include/phoenix_object.hpp>
+#include <boost/spirit/include/phoenix_operator.hpp>
+#include <boost/spirit/include/phoenix_stl.hpp>
+#include <boost/spirit/include/phoenix_bind.hpp>
+
 #include "parser.hpp"
+#include "ast.hpp"
 
 namespace dachs {
 namespace syntax {
 
-ast::ast parser::parse(std::string const&)
+using std::size_t;
+
+namespace detail {
+    template<class Iterator>
+    auto position_of(Iterator begin, Iterator current)
+    {
+        size_t line = 1;
+        size_t col = 1;
+        for (; begin != current; ++begin) {
+            if (*begin == '\n') {
+                col = 1;
+                line++;
+                continue;
+            }
+            col++;
+        }
+        return std::make_pair(line, col);
+    }
+} // namespace detail
+
+namespace qi = boost::spirit::qi;
+namespace phx = boost::phoenix;
+namespace ascii = boost::spirit::ascii;
+
+template<class Iterator>
+class grammar : public qi::grammar<Iterator, ast::node::program(), ascii::blank_type /*FIXME*/> {
+    template<class Value, class... Extra>
+    using rule = qi::rule<Iterator, Value, ascii::blank_type, Extra...>;
+
+public:
+    grammar() : grammar::base_type(program)
+    {
+        
+    }
+
+    ~grammar()
+    {}
+
+private:
+    rule<ast::node::program()> program;
+};
+
+ast::ast parser::parse(std::string const& code)
 {
-    return {};
+    auto itr = std::begin(code);
+    auto const end = std::end(code);
+    grammar<decltype(itr)> spiritual_parser;
+    ast::node::program root;
+
+    return {root};
 }
 
 } // namespace syntax
