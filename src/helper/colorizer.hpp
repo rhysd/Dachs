faf74253c2a4cb9ce80600cd3a2bf6dff90cc9bf
rhysd <lin90162@yahoo.co.jp>
2014-04-11 18:02:05 +0900

    some refatorings for colorizer
    - fix variable names
        - enable -> enabled
        - color_type -> brightness
        - add 'none' color

--- src/helper/colorizer.hpp
+++ src/helper/colorizer.hpp
@@ -6,9 +6,12 @@
 namespace dachs {
 namespace helper {
 
+// MAYBE TODO:
+// background color of text, underline, bold and blink effects
+
 template<class String>
 class colorizer {
-    bool enable;
+    bool enabled;
 
     enum class color {
         yellow,
@@ -18,9 +21,10 @@ class colorizer {
         cyan,
         purple,
         blue,
+        none,
     };
 
-    enum class color_type {
+    enum class brightness {
         light,
         dark,
     };
@@ -28,26 +32,30 @@ class colorizer {
     unsigned int color_table(color const c) const noexcept
     {
         switch(c) {
-        case color::yellow: return 93u;
-        case color::green:  return 92u;
         case color::gray:   return 90u;
         case color::red:    return 91u;
-        case color::cyan:   return 96u;
-        case color::purple: return 95u;
+        case color::green:  return 92u;
+        case color::yellow: return 93u;
         case color::blue:   return 94u;
+        case color::purple: return 95u;
+        case color::cyan:   return 96u;
+        case color::white:  return 97u;
+        case color::none:   return 0u;
         }
     }
 
-    std::string start_sequence(color_type const t, color const c) const noexcept {
+    std::string start_sequence(brightness const b, color const c) const noexcept {
         auto code = color_table(c);
-        if(t == color_type::dark) code -= 60;
+        if(b == brightness::dark && code >= 60u) {
+            code -= 60u;
+        }
         return "\033[" + std::to_string(code) + 'm';
     };
 
-    String colorize(color const c, String const& target, bool const end_sequence, color_type const t) const noexcept
+    String colorize(color const c, String const& target, bool const end_sequence, brightness const b) const noexcept
     {
-        if (enable) {
-            return start_sequence(t, c) + target + (end_sequence ? "\033[0m" : "");
+        if (enabled) {
+            return start_sequence(b, c) + target + (end_sequence ? "\033[0m" : "");
         } else {
             return target;
         }
@@ -56,36 +64,36 @@ class colorizer {
 public:
 
     explicit colorizer(bool const colorful_output=true)
-        : enable(colorful_output)
+        : enabled(colorful_output)
     {}
 
-    String yellow(String const& target, bool const end_seq=true, color_type const type=color_type::light) const noexcept
+    String yellow(String const& target, bool const end_seq=true, brightness const b=brightness::light) const noexcept
     {
-        return colorize(color::yellow, target, end_seq, type);
+        return colorize(color::yellow, target, end_seq, b);
     }
-    String green(String const& target, bool const end_seq=true, color_type const type=color_type::light) const noexcept
+    String green(String const& target, bool const end_seq=true, brightness const b=brightness::light) const noexcept
     {
-        return colorize(color::green, target, end_seq, type);
+        return colorize(color::green, target, end_seq, b);
     }
-    String gray(String const& target, bool const end_seq=true, color_type const type=color_type::light) const noexcept
+    String gray(String const& target, bool const end_seq=true, brightness const b=brightness::light) const noexcept
     {
-        return colorize(color::gray, target, end_seq, type);
+        return colorize(color::gray, target, end_seq, b);
     }
-    String red(String const& target, bool const end_seq=true, color_type const type=color_type::light) const noexcept
+    String red(String const& target, bool const end_seq=true, brightness const b=brightness::light) const noexcept
     {
-        return colorize(color::red, target, end_seq, type);
+        return colorize(color::red, target, end_seq, b);
     }
-    String cyan(String const& target, bool const end_seq=true, color_type const type=color_type::light) const noexcept
+    String cyan(String const& target, bool const end_seq=true, brightness const b=brightness::light) const noexcept
     {
-        return colorize(color::cyan, target, end_seq, type);
+        return colorize(color::cyan, target, end_seq, b);
     }
-    String purple(String const& target, bool const end_seq=true, color_type const type=color_type::light) const noexcept
+    String purple(String const& target, bool const end_seq=true, brightness const b=brightness::light) const noexcept
     {
-        return colorize(color::purple, target, end_seq, type);
+        return colorize(color::purple, target, end_seq, b);
     }
-    String blue(String const& target, bool const end_seq=true, color_type const type=color_type::light) const noexcept
+    String blue(String const& target, bool const end_seq=true, brightness const b=brightness::light) const noexcept
     {
-        return colorize(color::blue, target, end_seq, type);
+        return colorize(color::blue, target, end_seq, b);
     }
 };
 
