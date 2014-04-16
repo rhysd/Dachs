9ddcea964f6c09203fc4c1a4a6a9bb89d427df83
rhysd <lin90162@yahoo.co.jp>
2014-04-16 17:52:59 +0900

    add skelton for test with Boost.Test

---
+++ test/parser_test.cpp
@@ -0,0 +1,15 @@
+#define BOOST_TEST_MODULE ParserTest
+
+// #include "parser.hpp"
+
+#define BOOST_DYN_LINK
+#define BOOST_TEST_MAIN
+
+#include <boost/test/included/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE(parser)
+BOOST_AUTO_TEST_CASE(empty)
+{
+    BOOST_CHECK(true);
+}
+BOOST_AUTO_TEST_SUITE_END()
