531dc5ae384be21c4a1906dccb19377d3da1ed0f
rhysd <lin90162@yahoo.co.jp>
2014-05-15 03:01:48 +0900

    more utility for boost::variant

--- src/dachs/helper/variant.hpp
+++ src/dachs/helper/variant.hpp
@@ -1,12 +1,14 @@
 #if !defined DACHS_HELPER_VARIANT_HPP_INCLUDED
+
 #define      DACHS_HELPER_VARIANT_HPP_INCLUDED
+
+#include <utility>
+#include <type_traits>
 
 #include <boost/variant/variant.hpp>
 #include <boost/variant/static_visitor.hpp>
 #include <boost/variant/apply_visitor.hpp>
 #include <boost/optional.hpp>
-
-#include <utility>
 
 namespace dachs {
 namespace helper {
@@ -56,6 +58,47 @@ template<class T, class... Args>
 inline bool has(boost::variant<Args...> const& v)
 {
     return boost::apply_visitor(detail::type_checker<T>{}, v);
+}
+
+namespace detail {
+
+template<class F, class Result>
+struct lambda_wrapped_visitor : public boost::static_visitor<Result> {
+
+    F const& f;
+
+    explicit lambda_wrapped_visitor(F const& f)
+        : f(f)
+    {}
+
+    template<class T>
+    Result operator()(T const& t) const
+    {
+        return f(t);
+    }
+};
+
+template<class F>
+struct lambda_wrapped_visitor<F, void> : public boost::static_visitor<void> {
+    F const& f;
+
+    explicit lambda_wrapped_visitor(F const& f)
+        : f(f)
+    {}
+
+    template<class T>
+    void operator()(T const& t) const
+    {
+        f(t);
+    }
+};
+
+} // namespace detail
+
+template<class Lambda, class Head, class... Tail>
+inline auto apply_lambda(Lambda const& v, boost::variant<Head, Tail...> const& variant)
+{
+    return boost::apply_visitor(detail::lambda_wrapped_visitor<Lambda, typename std::result_of<Lambda(Head)>::type>{v}, variant);
 }
 
 } // namespace variant
