0603c67701af8d44a6a2afb3312ccd8e76f06d71
rhysd <lin90162@yahoo.co.jp>
2014-07-02 12:01:13 +0900

    add simple options for command line

--- src/main.cpp
+++ src/main.cpp
@@ -6,29 +6,21 @@
 #include "dachs/helper/util.hpp"
 #include "dachs/exception.hpp"
 
-int main(int const argc, char const* const argv[])
+template<class Action>
+int do_compiler_action(char const* const file, Action const& action)
 {
-    // TODO: Use Boost.ProgramOptions
-    //  --dump-ast
-    //  --dump-sym-table
-    //  --emit-llvm
-    if (argc != 2) {
-        std::cerr << "Usage: " << argv[0] << " {file}\n";
-        return 1;
-    }
-
-    auto const maybe_code = dachs::helper::read_file<std::string>(argv[1]);
+    auto const maybe_code = dachs::helper::read_file<std::string>(file);
     if (!maybe_code) {
-        std::cerr << "File cannot be opened: " << argv[1] << std::endl;
+        std::cerr << "File cannot be opened: " << file << std::endl;
         return 2;
     }
     auto const& code = *maybe_code;
     dachs::helper::colorizer<std::string> c;
 
-    dachs::compiler compiler;
     try {
-        compiler.compile(code);
+        action(code);
         std::cout << c.blue("〜完〜") << std::endl;
+        return 0;
     }
     catch (dachs::parse_error const& e) {
         std::cerr << e.what() << std::endl
@@ -43,16 +35,65 @@ int main(int const argc, char const* const argv[])
     catch (dachs::code_generation_error const& e) {
         std::cerr << e.what() << std::endl
                   << c.red("〜完〜") << std::endl;
+        return 5;
     }
     catch (dachs::not_implemented_error const& e) {
         std::cerr << e.what() << std::endl
                   << c.red("〜完〜") << std::endl;
-        return 5;
+        return 6;
     }
     catch (std::exception const& e) {
         std::cerr << "Internal compilation error: " << e.what() << std::endl;
         return -1;
     }
-
-    return 0;
 }
+
+int main(int const argc, char const* const argv[])
+{
+    auto const show_usage =
+        [=]()
+        {
+            std::cerr << "Usage: " << argv[0] << " [--dump-ast|--dump-sym-table|--emit-llvm] {file}\n";
+        };
+
+    // TODO: Use Boost.ProgramOptions
+
+    switch (argc) {
+        dachs::compiler compiler;
+
+    case 3: {
+        std::string const opt = argv[1];
+
+        if (opt == "--dump-ast") {
+            return do_compiler_action(
+                    argv[2],
+                    [&](auto const& s){ std::cout << compiler.dump_ast(s, true); }
+                );
+        } else if (opt == "--dump-sym-table") {
+            return do_compiler_action(
+                    argv[2],
+                    [&](auto const& s){ std::cout << compiler.dump_scopes(s); }
+                );
+        } else if (opt == "--emit-llvm") {
+            return do_compiler_action(
+                    argv[2],
+                    [&](auto const& s){ std::cout << compiler.dump_llvm_ir(s); }
+                );
+        } else {
+            show_usage();
+            return 1;
+        }
+    }
+
+    case 2:
+        return do_compiler_action(
+                argv[1],
+                [&](auto const& s){ compiler.compile(s); }
+            );
+
+    default:
+        show_usage();
+        return 1;
+    }
+}
+
