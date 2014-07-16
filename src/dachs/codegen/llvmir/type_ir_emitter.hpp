888cdb661f9bda67a0c26b4aa4200c41500bdfc2
rhysd <lin90162@yahoo.co.jp>
2014-07-17 01:26:10 +0900

    emit IR for tuple literals and accesses
    e.g.
        func main
            var c := 'a'
            t := (1, c, 3.14)
            print(t[0])
            print(t[1])
            print(t[2])
        end

--- src/dachs/codegen/llvmir/type_ir_emitter.hpp
+++ src/dachs/codegen/llvmir/type_ir_emitter.hpp
@@ -1,16 +1,150 @@
 #if !defined DACHS_CODEGEN_LLVMIR_TYPE_IR_GENERATOR_HPP_INCLUDED
 #define      DACHS_CODEGEN_LLVMIR_TYPE_IR_GENERATOR_HPP_INCLUDED
 
+#include <vector>
+
 #include <llvm/IR/LLVMContext.h>
-#include <llvm/IR/Type.h>
+#include <llvm/IR/DerivedTypes.h>
 
 #include "dachs/semantics/type.hpp"
+#include "dachs/codegen/llvmir/type_ir_emitter.hpp"
+#include "dachs/exception.hpp"
+#include "dachs/fatal.hpp"
+#include "dachs/helper/variant.hpp"
 
 namespace dachs {
 namespace codegen {
 namespace llvmir {
 
-llvm::Type *emit_type_ir(type::type const& t, llvm::LLVMContext &);
+class type_ir_emitter {
+    llvm::LLVMContext &context;
+
+    template<class String>
+    void error(String const& msg)
+    {
+        throw code_generation_error{"LLVM IR generator", msg};
+    }
+
+    template<class T, class String>
+    T check(T v, String const& msg)
+    {
+        if (!v) {
+            throw code_generation_error{"LLVM IR generator", msg};
+        }
+        return v;
+    }
+
+public:
+
+    explicit type_ir_emitter(llvm::LLVMContext &c)
+        : context(c)
+    {}
+
+    llvm::Type *emit(type::type const& any)
+    {
+        return helper::variant::apply_lambda(
+                [&](auto const& t){ return emit(t); }
+                , any.raw_value());
+    }
+
+    llvm::Type *emit(type::builtin_type const& builtin)
+    {
+        llvm::Type *result = nullptr;
+
+        if (builtin->name == "int") {
+            result = llvm::Type::getInt64Ty(context);
+        } else if (builtin->name == "uint") {
+            result = llvm::Type::getInt64Ty(context);
+        } else if (builtin->name == "float") {
+            result = llvm::Type::getDoubleTy(context);
+        } else if (builtin->name == "char") {
+            result = llvm::Type::getInt8Ty(context);
+        } else if (builtin->name == "bool") {
+            result = llvm::Type::getInt1Ty(context);
+        } else if (builtin->name == "string") {
+            result = llvm::Type::getInt8PtrTy(context);
+        } else if (builtin->name == "symbol") {
+            result = llvm::Type::getInt8PtrTy(context);
+        } else {
+            DACHS_RAISE_INTERNAL_COMPILATION_ERROR
+        }
+
+        if (!result) {
+            error("Failed to emit a builtin type: " + builtin->to_string());
+        }
+
+        return result;
+    }
+
+    llvm::Type *emit(type::class_type const&)
+    {
+        throw not_implemented_error{__FILE__, __func__, __LINE__, "class type"};
+    }
+
+    llvm::Type *emit(type::tuple_type const& t)
+    {
+        if (t->element_types.empty()) {
+            return llvm::Type::getVoidTy(context);
+        }
+
+        std::vector<llvm::Type *> element_type_irs;
+        element_type_irs.reserve(t->element_types.size());
+        for (auto const& t : t->element_types) {
+            element_type_irs.push_back(emit(t));
+        }
+
+        return check(
+            llvm::StructType::get(context, element_type_irs)
+                , "tuple type"
+            );
+    }
+
+    llvm::Type *emit(type::func_type const&)
+    {
+        throw not_implemented_error{__FILE__, __func__, __LINE__, "function type LLVM IR generation"};
+    }
+
+    llvm::Type *emit(type::proc_type const&)
+    {
+        throw not_implemented_error{__FILE__, __func__, __LINE__, "procedure type LLVM IR generation"};
+    }
+
+    llvm::Type *emit(type::func_ref_type const&)
+    {
+        throw not_implemented_error{__FILE__, __func__, __LINE__, "function reference type LLVM IR generation"};
+    }
+
+    llvm::Type *emit(type::dict_type const&)
+    {
+        throw not_implemented_error{__FILE__, __func__, __LINE__, "dictionary type LLVM IR generation"};
+    }
+
+    llvm::Type *emit(type::array_type const&)
+    {
+        throw not_implemented_error{__FILE__, __func__, __LINE__, "array type LLVM IR generation"};
+    }
+
+    llvm::Type *emit(type::range_type const&)
+    {
+        throw not_implemented_error{__FILE__, __func__, __LINE__, "range type LLVM IR generation"};
+    }
+
+    llvm::Type *emit(type::qualified_type const&)
+    {
+        throw not_implemented_error{__FILE__, __func__, __LINE__, "qualified type LLVM IR generation"};
+    }
+
+    llvm::Type *emit(type::template_type const&)
+    {
+        DACHS_RAISE_INTERNAL_COMPILATION_ERROR
+    }
+};
+
+template<class TypeType>
+llvm::Type *emit_type_ir(TypeType const& t, llvm::LLVMContext &context)
+{
+    return type_ir_emitter{context}.emit(t);
+}
 
 } // namespace llvmir
 } // namespace codegen
