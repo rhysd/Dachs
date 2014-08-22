89ae72a17e19db76598fa09c40844e0c0ea15559
rhysd <lin90162@yahoo.co.jp>
2014-08-22 19:19:38 +0900

    extend sed's regex to use '+'

--- test/output_error.sh
+++ test/output_error.sh
@@ -3,6 +3,7 @@
 GREEN=`echo '\033[92m'`
 RED=`echo '\033[91m'`
 YELLOW=`echo '\033[93m'`
+BLUE=`echo '\033[94m'`
 RESET=`echo '\033[0m'`
 
 start="s/^Start testing:/$YELLOW&$RESET/g;"
@@ -11,11 +12,15 @@ passed="s/^Test Passed\\.$/$GREEN&$RESET/g;"
 failed="s/^Test Failed\\.$/$RED&$RESET/g;"
 error="s/^[/._:[:alnum:]]+ error in \".+\":.*$/$RED&$RESET/g;"
 error2="s/fatal error in \".+\": .*$/$RED&$RESET/g;"
+info="s/\\*\\*\\*/$BLUE&$RESET/g;"
 
 exec cat "$(pwd)/Testing/Temporary/LastTest.log" 2>&1 |\
-    sed "$start\
-         $end\
-         $passed\
-         $failed\
-         $error\
-         $error2"
+    sed -E "\
+        $start\
+        $end\
+        $passed\
+        $failed\
+        $error\
+        $error2\
+        $info
+        "
