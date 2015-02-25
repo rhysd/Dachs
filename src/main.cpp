02c14abd2453eb8ce9938d087ae2257ac3f35dfc
rhysd <lin90162@yahoo.co.jp>
2015-02-26 03:21:35 +0900

    output "Caught deadly signal" message to stderr

--- src/main.cpp
+++ src/main.cpp
@@ -28,7 +28,7 @@ static void signal_handler(int s)
     dachs::helper::colorizer c;
     helper::backtrace_printer<20u> printer{c};
 
-    std::cout << c.red("Caught deadly signal " + std::to_string(s)) << "\n\n";
+    std::cerr << c.red("Caught deadly signal " + std::to_string(s)) << "\n\n";
     printer.dump_pretty_backtrace();
 
     std::cerr << std::endl;
