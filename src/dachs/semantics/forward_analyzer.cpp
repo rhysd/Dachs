b5c94332593223a6dc5c00131dda7fa095197c8b
rhysd <lin90162@yahoo.co.jp>
2015-02-27 01:00:05 +0900

    refactoring for defining built-in function

--- src/dachs/semantics/forward_analyzer.cpp
+++ src/dachs/semantics/forward_analyzer.cpp
@@ -2,6 +2,31 @@
 
 namespace dachs {
 namespace semantics {
+namespace detail {
+
+template<class T>
+inline auto make_global_func_param(char const* const name, T && type)
+{
+    auto p = symbol::make<symbol::var_symbol>(nullptr, name, true, true);
+    p->type = std::forward<T>(type);
+    return p;
+}
+
+template<class S, class T>
+inline auto make_global_func(S const& scope, char const* const name, T && ret)
+{
+    auto func = scope::make<scope::func_scope>(nullptr, scope, name, true);
+    func->body = scope::make<scope::local_scope>(func);
+    func->ret_type = std::forward<T>(ret);
+    auto func_var = symbol::make<symbol::var_symbol>(nullptr, name, true, true);
+    func_var->type = type::make<type::generic_func_type>(func);
+    func_var->is_global = true;
+    scope->define_function(func);
+    scope->force_define_constant(std::move(func_var));
+    return func;
+}
+
+} // namespace detail
 
 scope::scope_tree analyze_symbols_forward(ast::ast &a)
 {
@@ -13,75 +38,34 @@ scope::scope_tree analyze_symbols_forward(ast::ast &a)
 
         {
             // func print(str)
-            auto print_func = scope::make<scope::func_scope>(nullptr, scope_root, "print", true);
-            print_func->body = scope::make<scope::local_scope>(print_func);
-            print_func->ret_type = type::get_unit_type();
-            // Note: These definitions are never duplicate
-            auto p = symbol::make<symbol::var_symbol>(nullptr, "value", true, true);
-            p->type = dummy_template_type;
-            print_func->define_param(std::move(p));
-            scope_root->define_function(print_func);
-            auto func_var_sym = symbol::make<symbol::var_symbol>(nullptr, "print", true, true);
-            func_var_sym->type = type::make<type::generic_func_type>(print_func);
-            func_var_sym->is_global = true;
-            scope_root->force_define_constant(std::move(func_var_sym));
+            auto print_func = detail::make_global_func(scope_root, "print", type::get_unit_type());
+            print_func->define_param(detail::make_global_func_param("value", dummy_template_type));
         }
 
         {
             // func println(str)
-            auto println_func = scope::make<scope::func_scope>(nullptr, scope_root, "println", true);
-            println_func->body = scope::make<scope::local_scope>(println_func);
-            println_func->ret_type = type::get_unit_type();
-            // Note: These definitions are never duplicate
-            auto p = symbol::make<symbol::var_symbol>(nullptr, "value", true, true);
-            p->type = dummy_template_type;
-            println_func->define_param(std::move(p));
-            scope_root->define_function(println_func);
-            auto func_var_sym = symbol::make<symbol::var_symbol>(nullptr, "println", true, true);
-            func_var_sym->type = type::make<type::generic_func_type>(println_func);
-            func_var_sym->is_global = true;
-            scope_root->force_define_constant(std::move(func_var_sym));
+            auto println_func = detail::make_global_func(scope_root, "println", type::get_unit_type());
+            println_func->define_param(detail::make_global_func_param("value", dummy_template_type));
         }
 
         {
             // func read_cycle_counter()
-            auto read_cycle_counter_func = scope::make<scope::func_scope>(nullptr, scope_root, "__builtin_read_cycle_counter", true);
-            read_cycle_counter_func->body = scope::make<scope::local_scope>(read_cycle_counter_func);
-            read_cycle_counter_func->ret_type = type::get_builtin_type("uint");
-            scope_root->define_function(read_cycle_counter_func);
-            auto func_var_sym = symbol::make<symbol::var_symbol>(nullptr, read_cycle_counter_func->name, true, true);
-            func_var_sym->type = type::make<type::generic_func_type>(read_cycle_counter_func);
-            func_var_sym->is_global = true;
-            scope_root->force_define_constant(std::move(func_var_sym));
+            detail::make_global_func(scope_root, "__builtin_read_cycle_counter", type::get_builtin_type("uint"));
         }
 
         {
             // func address_of(x)
-            auto address_of_func = scope::make<scope::func_scope>(nullptr, scope_root, "__builtin_address_of", true);
-            address_of_func->body = scope::make<scope::local_scope>(address_of_func);
-            address_of_func->ret_type = type::get_builtin_type("uint");
-            auto param = symbol::make<symbol::var_symbol>(nullptr, "ptr", true, true);
-            param->type = dummy_template_type;
-            address_of_func->define_param(std::move(param));
-            scope_root->define_function(address_of_func);
-            auto func_var_sym = symbol::make<symbol::var_symbol>(nullptr, address_of_func->name, true, true);
-            func_var_sym->type = type::make<type::generic_func_type>(address_of_func);
-            func_var_sym->is_global = true;
-            scope_root->force_define_constant(std::move(func_var_sym));
+            auto address_of_func = detail::make_global_func(scope_root, "__builtin_address_of", type::get_builtin_type("uint"));
+            address_of_func->define_param(detail::make_global_func_param("ptr", dummy_template_type));
         }
 
         // Operators
         // cast functions
     }
 
-    {
-        // Builtin classes
-
-        // range
-    }
-        // Note:
-        // Check function duplication after generating scope tree because overload resolution requires
-        // of arguments and types of arguments require class information which should be forward-analyzed.
+    // Note:
+    // Check function duplication after generating scope tree because overload resolution requires
+    // of arguments and types of arguments require class information which should be forward-analyzed.
 
     return scope::scope_tree{analyze_ast_node_forward(a.root, scope_root)};
 }
