dc31a8158a46a2beaa553d52399c6e875aa2eb23
rhysd <lin90162@yahoo.co.jp>
2015-03-16 05:04:13 +0900

    add probable<T> to represent 'value or error'

---
+++ src/dachs/helper/probable.hpp
@@ -0,0 +1,251 @@
+#if !defined DACHS_HELPER_PROBABLE_HPP_INCLUDED
+#define      DACHS_HELPER_PROBABLE_HPP_INCLUDED
+
+#include <string>
+#include <type_traits>
+#include <boost/variant/variant.hpp>
+#include <boost/variant/get.hpp>
+#include <boost/optional.hpp>
+
+// Note:
+// dachs::helper::probable represents either a specified type or an error(usually std::string).
+// This class is used for a returned value which is the result of maybe-failed computation.
+// This is similar to Boost.Expected and should be replaced with it when it will be released.
+
+namespace dachs {
+namespace helper {
+
+namespace detail {
+    // Note:
+    // This class can't be in probable<T, E> because T must be specified
+    // in oops() free function.
+    template<class Err>
+    struct Error {
+        Err value;
+
+        bool operator==(Error const& rhs) const
+        {
+            return value == rhs.value;
+        }
+
+        bool operator!=(Error const& rhs) const
+        {
+            return value != rhs.value;
+        }
+
+        bool operator<(Error const& rhs) const
+        {
+            return value < rhs.value;
+        }
+    };
+} // namespace detail
+
+template<class T, class E = std::string>
+class probable {
+    static_assert(std::is_default_constructible<E>::value, "E must be default constructible");
+    using error_type = detail::Error<E>;
+    using element_type = T;
+    using value_type = boost::variant<element_type, error_type>;
+    value_type value;
+
+public:
+
+    using success_type = T;
+    using failure_type = E;
+
+    probable()
+        : value(error_type{})
+    {}
+
+    probable(probable && p)
+        : value(std::move(p).value)
+    {}
+
+    probable(probable const& p)
+        : value(p.value)
+    {}
+
+    template<class U>
+    probable(probable<U, E> & p)
+        : value(p.raw_value())
+    {}
+
+    template<class U>
+    probable(probable<U, E> && p)
+        : value(std::move(p.raw_value()))
+    {}
+
+    template<class U>
+    probable(probable<U, E> const& p)
+        : value(p.raw_value())
+    {}
+
+    template<class U>
+    probable(U && v)
+        : value(std::forward<U>(v))
+    {}
+
+    probable &operator=(probable && rhs)
+    {
+        value = std::move(rhs).value;
+        return *this;
+    }
+
+    probable &operator=(probable const& rhs)
+    {
+        value = rhs.value;
+        return *this;
+    }
+
+    template<class U>
+    probable &operator=(U && rhs)
+    {
+        value = std::forward<U>(rhs);
+        return *this;
+    }
+
+    bool operator==(probable const& rhs) const
+    {
+        return value == rhs.value;
+    }
+
+    template<class U>
+    bool operator==(U const& rhs) const
+    {
+        return value == rhs;
+    }
+
+    template<class U>
+    bool operator!=(U const& rhs) const
+    {
+        return !(*this == rhs);
+    }
+
+    bool operator<(probable const& rhs) const
+    {
+        return value < rhs.value;
+    }
+
+    template<class U>
+    bool operator<(U const& rhs) const
+    {
+        return value < rhs;
+    }
+
+    bool failure() const
+    {
+        return value.which() == 1;
+    }
+
+    bool success() const
+    {
+        return value.which() == 0;
+    }
+
+    boost::optional<element_type> get() const
+    {
+        element_type const* const ptr = boost::get<element_type>(&value);
+        if (ptr) {
+            return {*ptr};
+        } else {
+            return boost::none;
+        }
+    }
+
+    element_type &get_unsafe()
+    {
+        return *boost::get<element_type>(&value);
+    }
+
+    element_type const& get_unsafe() const
+    {
+        return *boost::get<element_type>(&value);
+    }
+
+    boost::optional<E> get_error() const
+    {
+        error_type const* const ptr = boost::get<error_type>(&value);
+        if (ptr) {
+            return {ptr->value};
+        } else {
+            return boost::none;
+        }
+    }
+
+    E &get_error_unsafe()
+    {
+        return boost::get<error_type>(&value)->value;
+    }
+
+    E const& get_error_unsafe() const
+    {
+        return boost::get<error_type>(&value)->value;
+    }
+
+    value_type &raw_value()
+    {
+        return value;
+    }
+
+    value_type const& raw_value() const
+    {
+        return value;
+    }
+
+    boost::variant<element_type, E> get_value_or_error() const
+    {
+        if (auto const* v = boost::get<element_type>(&value)) {
+            return *v;
+        } else if (auto const* v = boost::get<E>(&value)) {
+            return *v;
+        } else {
+            return {};
+        }
+    }
+};
+
+template<class T, class... Args>
+inline probable<T, std::string> make_probable(Args &&... args)
+{
+    return probable<T, std::string>(T(std::forward<Args>(args)...));
+}
+
+template<class S>
+inline typename std::enable_if<
+        std::is_constructible<std::string, decltype(std::forward<S>(std::declval<S>()))>::value,
+        detail::Error<std::string>
+    >::type
+oops(S && err)
+{
+    return detail::Error<std::string>{std::forward<S>(err)};
+}
+
+template<class E>
+inline typename std::enable_if<
+        !std::is_constructible<std::string, decltype(std::forward<E>(std::declval<E>()))>::value,
+        detail::Error<E>
+    >::type
+oops(E && e)
+{
+    return detail::Error<E>{std::forward<E>(e)};
+}
+
+template<class E = std::string, class E1, class E2, class... EN>
+inline detail::Error<E> oops(E1 && e1, E2 && e2, EN &&... en)
+{
+    return detail::Error<E>{std::forward<E1>(e1), std::forward<E2>(e2), std::forward<EN>(en)...};
+}
+
+template<class T>
+auto make_probable_generator()
+{
+    return [](auto &&... args){
+        return make_probable<T>(std::forward<decltype(args)>(args)...);
+    };
+}
+
+
+} // namespace helper
+} // namespace dachs
+
+#endif    // DACHS_HELPER_PROBABLE_HPP_INCLUDED
---
+++ test/helper_test.cpp
@@ -0,0 +1,133 @@
+#define BOOST_TEST_MODULE Util
+#define BOOST_DYN_LINK
+#define BOOST_TEST_MAIN
+
+#include <type_traits>
+#include <string>
+
+#include <boost/test/included/unit_test.hpp>
+
+#include "dachs/helper/probable.hpp"
+
+using namespace dachs::helper;
+
+BOOST_AUTO_TEST_SUITE(helper)
+
+BOOST_AUTO_TEST_SUITE(helper)
+
+BOOST_AUTO_TEST_CASE(string_error)
+{
+    enum class test {
+        foo, bar, baz
+    };
+
+    static_assert(std::is_same<typename probable<int>::success_type, int>::value, "");
+    static_assert(std::is_same<typename probable<int>::failure_type, std::string>::value, "");
+    static_assert(std::is_same<typename probable<int, char>::failure_type, char>::value, "");
+
+    auto const p1 = make_probable<test>(test::bar);
+    BOOST_CHECK(p1.success());
+    BOOST_CHECK(!p1.failure());
+
+    auto const r1 = p1.get();
+    BOOST_CHECK(r1);
+    BOOST_CHECK(*r1 == test::bar);
+    BOOST_CHECK(p1.get_unsafe() == test::bar);
+    BOOST_CHECK(p1.raw_value().which() == 0u);
+
+    auto const f1 = p1.get_error();
+    BOOST_CHECK(!f1);
+
+    probable<test> const p2 = oops("error occurred!");
+    BOOST_CHECK(!p2.success());
+    BOOST_CHECK(p2.failure());
+
+    auto const r2 = p2.get();
+    BOOST_CHECK(!r2);
+
+    auto const f2 = p2.get_error();
+    BOOST_CHECK(f2);
+    BOOST_CHECK(*f2 == "error occurred!");
+    BOOST_CHECK(p2.get_error_unsafe() == "error occurred!");
+    BOOST_CHECK(p2.raw_value().which() == 1u);
+
+    auto p3 = make_probable<test>(test::baz);
+    p3 = test::foo;
+    auto foo = test::foo;
+    p3 = foo;
+    auto const foo2 = foo;
+    p3 = foo2;
+
+    auto p4 = p3;
+    probable<test> p5 = test::foo;
+    BOOST_CHECK(p5.success());
+
+    BOOST_CHECK(p4 == p5);
+    BOOST_CHECK(p1 != p5);
+    BOOST_CHECK(p2 != p5);
+    BOOST_CHECK(p2 == p2);
+    BOOST_CHECK(p5 < p1);
+
+    auto const g = make_probable_generator<test>();
+    auto const p6 = g(test::foo);
+    BOOST_CHECK(p6.success());
+}
+
+BOOST_AUTO_TEST_CASE(user_defined_error)
+{
+    enum class test {
+        foo, bar, baz
+    };
+
+    enum class error {
+        err1, err2, err3
+    };
+
+    using test_type = probable<test, error>;
+
+    test_type const p1 = test::bar;
+    BOOST_CHECK(p1.success());
+    BOOST_CHECK(!p1.failure());
+
+    auto const r1 = p1.get();
+    BOOST_CHECK(r1);
+    BOOST_CHECK(*r1 == test::bar);
+    BOOST_CHECK(p1.get_unsafe() == test::bar);
+    BOOST_CHECK(p1.raw_value().which() == 0u);
+
+    auto const f1 = p1.get_error();
+    BOOST_CHECK(!f1);
+
+    test_type const p2 = oops<error>(error::err1);
+    BOOST_CHECK(!p2.success());
+    BOOST_CHECK(p2.failure());
+
+    auto const r2 = p2.get();
+    BOOST_CHECK(!r2);
+
+    auto const f2 = p2.get_error();
+    BOOST_CHECK(f2);
+    BOOST_CHECK(*f2 == error::err1);
+    BOOST_CHECK(p2.get_error_unsafe() == error::err1);
+    BOOST_CHECK(p2.raw_value().which() == 1u);
+}
+
+BOOST_AUTO_TEST_CASE(edge_case_string)
+{
+    auto const p = make_probable<std::string>("aaaa");
+    BOOST_CHECK(p.success());
+    auto const r = p.get();
+    BOOST_CHECK(r);
+    BOOST_CHECK(*r == "aaaa");
+    auto const v = p.get_value_or_error();
+
+    probable<std::string> const p2 = oops("bbbb");
+    BOOST_CHECK(p2.failure());
+    auto const f = p2.get_error();
+    BOOST_CHECK(f);
+    BOOST_CHECK(*f == "bbbb");
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+BOOST_AUTO_TEST_SUITE_END()
